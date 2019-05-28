58071740c52912cd2c0f04981f452fbd3afd4265
Monty Khanna <mackraja@users.noreply.github.com>
2019-05-28 13:56:51 +0530

    update config of production

--- config/db.js
+++ config/db.js
@@ -33,23 +33,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'flsunstohvjtrs',
-    password: '810548be41f34ecf0847ebb53be4cfccbfa4e9cacea83081ff6bd6a3cadddea0',
-    database: 'd3b3qj3j2rvm5',
-    host: 'ec2-54-163-230-199.compute-1.amazonaws.com',
+    use_env_variable: 'DATABASE_URL',
     ssl: true,
     dialect: 'postgres',
     dialectOptions: {
       ssl: true
-    },
-    connectionTimeout: 300000,
-    requestTimeout: 300000,
-    pool: {
-      min: 20,
-      max: 30,
-      idle: 30000,
-      acquire: 300000,
-      idleTimeoutMillis: 300000,
-    },
+    }
   },
 };
