edbbe41e06e4067faec6731298507d3f93e133e0
Monty Khanna <monty.khanna@simsaw.com>
2019-05-28 15:05:04 +0530

    Updated config

--- config/db.js
+++ config/db.js
@@ -3,29 +3,17 @@
  */
 module.exports = {
   development: {
-    use_env_variable: 'postgres://flsunstohvjtrs:810548be41f34ecf0847ebb53be4cfccbfa4e9cacea83081ff6bd6a3cadddea0@ec2-54-163-230-199.compute-1.amazonaws.com:5432/d3b3qj3j2rvm5',
-    username: 'flsunstohvjtrs',
-    password: '810548be41f34ecf0847ebb53be4cfccbfa4e9cacea83081ff6bd6a3cadddea0',
-    database: 'd3b3qj3j2rvm5',
-    host: 'ec2-54-163-230-199.compute-1.amazonaws.com',
-    ssl: true,
-    logging: true,
+    username: 'postgres',
+    password: 'Bohemia007',
+    database: 'gettingStarted',
+    host: 'localhost',
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-      ssl: true
-    },
-    // username: 'postgres',
-    // password: 'Bohemia007',
-    // database: 'gettingStarted',
-    // host: 'localhost',
-    // dialect: 'postgres',
-    // protocol: 'postgres',
     connectionTimeout: 300000,
     requestTimeout: 300000,
     pool: {
-      min: 0,
-      max: 20,
+      min: 20,
+      max: 30,
       idle: 30000,
       acquire: 300000,
       idleTimeoutMillis: 300000,
--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
  * @author {[Monty Khanna]}
  */
 export default {
-  apiHost: process.env.API_HOST || 'https://ec2-54-163-230-199.compute-1.amazonaws.com:5432' // 'http://localhost:4000'
+  apiHost: process.env.API_HOST || 'http://localhost:4000'
 };
